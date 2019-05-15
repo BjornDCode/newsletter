8367c96c04951b6ecfa5b57dd1a37cbbe0ec4c35
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2019-05-15 19:16:36 +0200

    Add Eslint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    "es6": true
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  }
+};

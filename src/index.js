7a1cbaf86b2a3f6d7b16ed7deebb9730c8250ec3
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2019-05-16 20:20:41 +0200

    Add docblock

--- src/index.js
+++ src/index.js
@@ -8,42 +8,47 @@ const InvalidOptionsError = require('./errors/InvalidOptionsError')
 const InvalidProviderError = require('./errors/InvalidProviderError')
 const InvalidKeyError = require('./errors/InvalidKeyError')
 
-module.exports = {
-    create: (options = {}) => {
-        if (!isObject(options)) {
-            throw new InvalidOptionsError('Options must be an object')
-        }
-
-        if (isEmpty(options)) {
-            throw new InvalidOptionsError(
-                'You must provide .create with an object of options'
-            )
-        }
-
-        if (!options.provider) {
-            throw new InvalidProviderError(
-                'You must specify a provider in the options'
-            )
-        }
-
-        if (!isString(options.provider)) {
-            throw new InvalidProviderError('The provider must be a string')
-        }
-
-        if (PROVIDERS.indexOf(options.provider) === -1) {
-            throw new InvalidProviderError('The provider is not supported')
-        }
-
-        if (!options.key) {
-            throw new InvalidKeyError(
-                'You must specify an api key in the options'
-            )
-        }
-
-        if (!isString(options.key)) {
-            throw new InvalidKeyError('The api key must be a string')
-        }
-
-        return new Newsletter()
+/**
+ * Creates an instance of Newsletter
+ * @param  {Object} Options for initialisation
+ * @return {Newsletter} An instance of Newsletter
+ */
+const create = (options = {}) => {
+    if (!isObject(options)) {
+        throw new InvalidOptionsError('Options must be an object')
     }
+
+    if (isEmpty(options)) {
+        throw new InvalidOptionsError(
+            'You must provide .create with an object of options'
+        )
+    }
+
+    if (!options.provider) {
+        throw new InvalidProviderError(
+            'You must specify a provider in the options'
+        )
+    }
+
+    if (!isString(options.provider)) {
+        throw new InvalidProviderError('The provider must be a string')
+    }
+
+    if (PROVIDERS.indexOf(options.provider) === -1) {
+        throw new InvalidProviderError('The provider is not supported')
+    }
+
+    if (!options.key) {
+        throw new InvalidKeyError('You must specify an api key in the options')
+    }
+
+    if (!isString(options.key)) {
+        throw new InvalidKeyError('The api key must be a string')
+    }
+
+    return new Newsletter()
+}
+
+module.exports = {
+    create
 }
